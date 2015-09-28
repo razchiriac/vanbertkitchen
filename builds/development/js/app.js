fc7f6177787ec73118361bda8b933c417d11cbe9
Raz <raz.chiriac@gmail.com>
2015-09-27 22:12:46 -0400

    cabinets page

--- builds/development/js/app.js
+++ builds/development/js/app.js
@@ -8,138 +8,172 @@ var myApp = angular.module('myApp', [
 var appControllers = angular.module('appControllers', ['firebase']);
 
 myApp.run(["$rootScope", "$state", function ($rootScope, $state) {
-    $rootScope.$on("$stateChangeError", function (event, toState, toParams, fromState, fromParams, error) {
-        // We can catch the error thrown when the $requireAuth promise is rejected
-        // and redirect the user back to the home page
-        if (error === "AUTH_REQUIRED") {
-            $state.go("home");
-        }
-    });
+	$rootScope.$on("$stateChangeError", function (event, toState, toParams, fromState, fromParams, error) {
+		// We can catch the error thrown when the $requireAuth promise is rejected
+		// and redirect the user back to the home page
+		if (error === "AUTH_REQUIRED") {
+			$state.go("home");
+		}
+	});
 }]);
 
 
 myApp.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
 
-    Stripe.setPublishableKey('pk_test_PTQxPyzUAucunSs2MkXAuaPo');
+	Stripe.setPublishableKey('pk_test_PTQxPyzUAucunSs2MkXAuaPo');
 
-    // For any unmatched url, send to /route1
-    $urlRouterProvider.otherwise("/home")
+	// For any unmatched url, send to /route1
+	$urlRouterProvider.otherwise("/home")
 
-    $stateProvider
-        .state('home', {
-            url: '/home',
-            templateUrl: 'views/home.html'
-        })
-        /* LOGIN & REGISTER */
-        .state('login', {
-            url: '/login',
-            templateUrl: 'views/login.html',
-            controller: 'RegistrationController'
-        })
-        .state('register', {
-            url: '/register',
-            templateUrl: 'views/register.html',
-            controller: 'RegistrationController'
-        })
-        /* END OF LOGIN & REGISTER */
-        .state('admin', {
-            url: '/admin',
-            templateUrl: 'views/admin.html',
-            resolve: {
-                // controller will not be loaded until $requireAuth resolves
-                // Auth refers to our $firebaseAuth wrapper in the example above
-                "currentAuth": ["Auth", function (Auth) {
-                    // $requireAuth returns a promise so the resolve waits for it to complete
-                    // If the promise is rejected, it will throw a $stateChangeError (see above)
-                    return Auth.$requireAuth();
+	$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: 'views/home.html'
+		})
+		/* LOGIN & REGISTER */
+		.state('login', {
+			url: '/login',
+			templateUrl: 'views/login.html',
+			controller: 'RegistrationController'
+		})
+		.state('register', {
+			url: '/register',
+			templateUrl: 'views/register.html',
+			controller: 'RegistrationController'
+		})
+		/* END OF LOGIN & REGISTER */
+		.state('admin', {
+			url: '/admin',
+			templateUrl: 'views/admin.html',
+			resolve: {
+				// controller will not be loaded until $requireAuth resolves
+				// Auth refers to our $firebaseAuth wrapper in the example above
+				"currentAuth": ["Auth", function (Auth) {
+					// $requireAuth returns a promise so the resolve waits for it to complete
+					// If the promise is rejected, it will throw a $stateChangeError (see above)
+					return Auth.$requireAuth();
       }]
-            }
-        })
-        .state('admin.doors', {
-            url: '/admin-doors',
-            templateUrl: 'views/admin.doors.html',
-            controller: 'DoorsController',
-            resolve: {
-                // controller will not be loaded until $requireAuth resolves
-                // Auth refers to our $firebaseAuth wrapper in the example above
-                "currentAuth": ["Auth", function (Auth) {
-                    // $requireAuth returns a promise so the resolve waits for it to complete
-                    // If the promise is rejected, it will throw a $stateChangeError (see above)
-                    return Auth.$requireAuth();
+			}
+		})
+		.state('admin.doors', {
+			url: '/admin-doors',
+			templateUrl: 'views/admin.doors.html',
+			controller: 'DoorsController',
+			resolve: {
+				// controller will not be loaded until $requireAuth resolves
+				// Auth refers to our $firebaseAuth wrapper in the example above
+				"currentAuth": ["Auth", function (Auth) {
+					// $requireAuth returns a promise so the resolve waits for it to complete
+					// If the promise is rejected, it will throw a $stateChangeError (see above)
+					return Auth.$requireAuth();
       }]
-            }
-        })
-        .state('admin.cabinets', {
-            url: '/admin-cabinets',
-            templateUrl: 'views/admin.cabinets.html',
-            controller: 'CabinetsController',
-            resolve: {
-                // controller will not be loaded until $requireAuth resolves
-                // Auth refers to our $firebaseAuth wrapper in the example above
-                "currentAuth": ["Auth", function (Auth) {
-                    // $requireAuth returns a promise so the resolve waits for it to complete
-                    // If the promise is rejected, it will throw a $stateChangeError (see above)
-                    return Auth.$requireAuth();
+			}
+		})
+		.state('admin.cabinets', {
+			url: '/admin-cabinets',
+			templateUrl: 'views/admin.cabinets.html',
+			controller: 'CabinetsController',
+			resolve: {
+				// controller will not be loaded until $requireAuth resolves
+				// Auth refers to our $firebaseAuth wrapper in the example above
+				"currentAuth": ["Auth", function (Auth) {
+					// $requireAuth returns a promise so the resolve waits for it to complete
+					// If the promise is rejected, it will throw a $stateChangeError (see above)
+					return Auth.$requireAuth();
       }]
-            }
-        })
-        /* DOORS */
-        .state('doors', {
-            url: '/doors',
-            templateUrl: 'views/doors.html',
-            controller: 'DoorsController'
-        })
-        .state('doors.category', {
-            url: '/category',
-            templateUrl: 'views/doors.category.html',
-            controller: 'DoorsController'
-        })
-        .state('doors.style', {
-            url: '/style',
-            templateUrl: 'views/doors.style.html',
-            controller: 'DoorsController'
-        })
-        .state('doors.color', {
-            url: '/color',
-            templateUrl: 'views/doors.color.html',
-            controller: 'DoorsController'
-        })
-        .state('doors.door', {
-            url: '/door',
-            templateUrl: 'views/doors.door.html',
-            controller: 'DoorsController'
-        })
-        .state('doors.all', {
-            url: '/all',
-            templateUrl: 'views/doors.all.html',
-            controller: 'DoorsController'
-        })
-        /* END OF DOORS */
-        /* CABINETS */
-        /* END OF CABINETS */
-        /* CHECKOUT */
+			}
+		})
+		/* DOORS */
+		.state('doors', {
+			url: '/doors',
+			templateUrl: 'views/doors.html',
+			controller: 'DoorsController'
+		})
+		.state('doors.category', {
+			url: '/category',
+			templateUrl: 'views/doors.category.html',
+			controller: 'DoorsController'
+		})
+		.state('doors.style', {
+			url: '/style',
+			templateUrl: 'views/doors.style.html',
+			controller: 'DoorsController'
+		})
+		.state('doors.color', {
+			url: '/color',
+			templateUrl: 'views/doors.color.html',
+			controller: 'DoorsController'
+		})
+		.state('doors.door', {
+			url: '/door',
+			templateUrl: 'views/doors.door.html',
+			controller: 'DoorsController'
+		})
+		.state('doors.all', {
+			url: '/all',
+			templateUrl: 'views/doors.all.html',
+			controller: 'DoorsController'
+		})
+		/* END OF DOORS */
+		/* CABINETS */
+		.state('cabinets', {
+			url: '/cabinets',
+			templateUrl: 'views/cabinets.html',
+			controller: 'CabinetsController'
+		})
+		.state('cabinets.categories', {
+			url: '/cabinet-categories',
+			templateUrl: 'views/cabinets.categories.html',
+			controller: 'CabinetsController'
+		})
+		.state('cabinets.styles', {
+			url: '/cabinet-styles',
+			templateUrl: 'views/cabinets.styles.html',
+			controller: 'CabinetsController'
+		})
+		.state('cabinets.all', {
+			url: '/cabinets',
+			templateUrl: 'views/cabinets.all.html',
+			controller: 'CabinetsController'
+		})
+		/* END OF CABINETS */
+		/* CHECKOUT */
 
-    .state('checkout', {
-        url: '/checkout',
-        templateUrl: 'views/checkout.html',
-        controller: 'StripeController'
-    })
+	.state('checkout', {
+		url: '/checkout',
+		templateUrl: 'views/checkout.html',
+		controller: 'StripeController'
+	})
 
-    /* END OF CHECKOUT */
+	/* END OF CHECKOUT */
 
 
 }]);
 
 myApp.directive('myEnter', function () {
-    return function (scope, element, attrs) {
-        element.bind("keydown keypress", function (event) {
-            if (event.which === 13) {
-                scope.$apply(function () {
-                    scope.$eval(attrs.myEnter);
-                });
+	return function (scope, element, attrs) {
+		element.bind("keydown keypress", function (event) {
+			if (event.which === 13) {
+				scope.$apply(function () {
+					scope.$eval(attrs.myEnter);
+				});
 
-                event.preventDefault();
-            }
-        });
-    };
+				event.preventDefault();
+			}
+		});
+	};
+});
+
+myApp.filter('showByCategory', function () {
+	return function (input) {
+		var out = [];
+		angular.forEach(input, function (product) {
+			if (product.name === 'all') {
+				out.push(product);
+			} else if (product.category.name === input) {
+				out.push(product);
+			}
+		})
+		return out;
+	}
 });
--- builds/development/js/controllers/cabinets.js
+++ builds/development/js/controllers/cabinets.js
@@ -1,218 +1,230 @@
 myApp.controller('CabinetsController', ['$scope', '$rootScope', '$firebase', '$firebaseObject', '$firebaseArray', 'FIREBASE_URL',
 	function ($scope, $rootScope, $firebase, $firebaseObject, $firebaseArray, FIREBASE_URL) {
 
-        var refCabinets = new Firebase(FIREBASE_URL + '/products/cabinets');
-        var refCabinetCategories = new Firebase(FIREBASE_URL + '/products/cabinet-categories');
-        var refCabinetStyles = new Firebase(FIREBASE_URL + '/products/cabinet-styles');
-        var refCabinetColors = new Firebase(FIREBASE_URL + '/products/cabinet-colors');
+		var refCabinets = new Firebase(FIREBASE_URL + '/products/cabinets');
+		var refCabinetCategories = new Firebase(FIREBASE_URL + '/products/cabinet-categories');
+		var refCabinetStyles = new Firebase(FIREBASE_URL + '/products/cabinet-styles');
+		var refCabinetColors = new Firebase(FIREBASE_URL + '/products/cabinet-colors');
 
-        var cabinetsObj = $firebaseObject(refCabinets);
-        var cabinetsArr = $firebaseArray(refCabinets);
+		var cabinetsObj = $firebaseObject(refCabinets);
+		var cabinetsArr = $firebaseArray(refCabinets);
 
-        var cabinetCategoriesObj = $firebaseObject(refCabinetCategories);
-        var cabinetCategoriesArr = $firebaseArray(refCabinetCategories);
+		var cabinetCategoriesObj = $firebaseObject(refCabinetCategories);
+		var cabinetCategoriesArr = $firebaseArray(refCabinetCategories);
 
-        var cabinetStylesObj = $firebaseObject(refCabinetStyles);
-        var cabinetStylesArr = $firebaseArray(refCabinetStyles);
+		var cabinetStylesObj = $firebaseObject(refCabinetStyles);
+		var cabinetStylesArr = $firebaseArray(refCabinetStyles);
 
-        var cabinetColorsObj = $firebaseObject(refCabinetColors);
-        var cabinetColorsArr = $firebaseArray(refCabinetColors);
+		var cabinetColorsObj = $firebaseObject(refCabinetColors);
+		var cabinetColorsArr = $firebaseArray(refCabinetColors);
 
-        cabinetsObj.$loaded().then(function (data) {
-            $scope.cabinets = data;
-            cabinetsObj.$bindTo($scope, 'cabinets');
-        }); // make sure cabinets object data has loaded
+		cabinetsObj.$loaded().then(function (data) {
+			$scope.cabinets = data;
+			cabinetsObj.$bindTo($scope, 'cabinets');
+		}); // make sure cabinets object data has loaded
 
-        cabinetsArr.$loaded().then(function (data) {
-            $scope.cabinetsArr = data;
-            cabinetsArr.$bindTo($scope, 'cabinetsArray');
-        }); // make sure cabinets array data has loaded
+		cabinetsArr.$loaded().then(function (data) {
+			$scope.cabinetsArr = data;
+			cabinetsArr.$bindTo($scope, 'cabinetsArray');
+		}); // make sure cabinets array data has loaded
 
-        cabinetCategoriesObj.$loaded().then(function (data) {
-            $scope.cabinetCategories = data;
-            cabinetCategoriesObj.$bindTo($scope, 'cabinetCategories');
-        }); // make sure cabinets object data has loaded
+		cabinetCategoriesObj.$loaded().then(function (data) {
+			$scope.cabinetCategories = data;
+			cabinetCategoriesObj.$bindTo($scope, 'cabinetCategories');
+		}); // make sure cabinets object data has loaded
 
-        cabinetCategoriesArr.$loaded().then(function (data) {
-            $scope.cabinetCategoriesArr = data;
-            cabinetCategoriesArr.$bindTo($scope, 'cabinetCategoriesArray');
-        }); // make sure cabinets array data has loaded
+		cabinetCategoriesArr.$loaded().then(function (data) {
+			$scope.cabinetCategoriesArr = data;
+			cabinetCategoriesArr.$bindTo($scope, 'cabinetCategoriesArray');
+		}); // make sure cabinets array data has loaded
 
-        cabinetStylesObj.$loaded().then(function (data) {
-            $scope.cabinetStyles = data;
-            cabinetStylesObj.$bindTo($scope, 'cabinetStyles');
-        }); // make sure cabinets object data has loaded
+		cabinetStylesObj.$loaded().then(function (data) {
+			$scope.cabinetStyles = data;
+			cabinetStylesObj.$bindTo($scope, 'cabinetStyles');
+		}); // make sure cabinets object data has loaded
 
-        cabinetStylesArr.$loaded().then(function (data) {
-            $scope.cabinetStylesArr = data;
-            cabinetStylesArr.$bindTo($scope, 'cabinetStylesArray');
-        }); // make sure cabinets array data has loaded
+		cabinetStylesArr.$loaded().then(function (data) {
+			$scope.cabinetStylesArr = data;
+			cabinetStylesArr.$bindTo($scope, 'cabinetStylesArray');
+		}); // make sure cabinets array data has loaded
 
-        cabinetColorsObj.$loaded().then(function (data) {
-            $scope.cabinetColors = data;
-            cabinetColorsObj.$bindTo($scope, 'cabinetColors');
-        }); // make sure cabinets object data has loaded
+		cabinetColorsObj.$loaded().then(function (data) {
+			$scope.cabinetColors = data;
+			cabinetColorsObj.$bindTo($scope, 'cabinetColors');
+		}); // make sure cabinets object data has loaded
 
-        cabinetColorsArr.$loaded().then(function (data) {
-            $scope.cabinetColorsArr = data;
-            cabinetColorsArr.$bindTo($scope, 'cabinetColorsArray');
-        }); // make sure cabinets array data has loaded
+		cabinetColorsArr.$loaded().then(function (data) {
+			$scope.cabinetColorsArr = data;
+			cabinetColorsArr.$bindTo($scope, 'cabinetColorsArray');
+		}); // make sure cabinets array data has loaded
 
-        $scope.tempCabinetColors = [];
+		$scope.tempCabinetColors = [];
 
-        $scope.addColorToCabinet = function (color) {
-            $scope.tempCabinetColors.push(color);
-        };
+		$scope.addColorToCabinet = function (color) {
+			$scope.tempCabinetColors.push(color);
+		};
 
-        $scope.removeColorFromCabinet = function (color) {
-            var index = $scope.tempCabinetColors.indexOf(color);
-            if (index > -1) {
-                $scope.tempCabinetColors.splice(index, 1);
-            }
-        };
+		$scope.removeColorFromCabinet = function (color) {
+			var index = $scope.tempCabinetColors.indexOf(color);
+			if (index > -1) {
+				$scope.tempCabinetColors.splice(index, 1);
+			}
+		};
 
-        $scope.addCabinet = function () {
-            var imgUrl = 'http://placehold.it/190x305';
-            $scope.cabinetImageUrl = imgUrl;
-            var tempCabinet = {
-                name: $scope.cabinetname,
-                category: $scope.selectedCat,
-                style: $scope.selectedStyle,
-                colors: $scope.tempCabinetColors,
-                minWidth: $scope.cabinetWidth.min,
-                maxWidth: $scope.cabinetWidth.max,
-                minHeight: $scope.cabinetHeight.min,
-                maxHeight: $scope.cabinetHeight.max,
-                minDepth: $scope.cabinetDepth.min,
-                maxDepth: $scope.cabinetDepth.max,
-                price: $scope.cabinetprice,
-                imageUrl: $scope.cabinetImageUrl,
-                date: Firebase.ServerValue.TIMESTAMP
-            };
+		$scope.addCabinet = function () {
+			var imgUrl = 'http://placehold.it/190x305';
+			$scope.cabinetImageUrl = imgUrl;
+			var tempCabinet = {
+				name: $scope.cabinetname,
+				category: $scope.selectedCat,
+				style: $scope.selectedStyle,
+				colors: $scope.tempCabinetColors,
+				minWidth: $scope.cabinetWidth.min,
+				maxWidth: $scope.cabinetWidth.max,
+				minHeight: $scope.cabinetHeight.min,
+				maxHeight: $scope.cabinetHeight.max,
+				minDepth: $scope.cabinetDepth.min,
+				maxDepth: $scope.cabinetDepth.max,
+				price: $scope.cabinetprice,
+				imageUrl: $scope.cabinetImageUrl,
+				date: Firebase.ServerValue.TIMESTAMP
+			};
 
-            cabinetsArr.$add(tempCabinet).then(function (ref) {
-                $scope.cabinetname = '';
-                $scope.selectedCat = '';
-                $scope.selectedStyle = '';
-                $scope.tempCabinetColors = '';
-                $scope.cabinetWidth.min = '';
-                $scope.cabinetWidth.max = '';
-                $scope.cabinetHeight.min = '';
-                $scope.cabinetHeight.max = '';
-                $scope.cabinetDepth.min = '';
-                $scope.cabinetDepth.max = '';
-                $scope.cabinetprice = '';
-                $scope.cabinetImageUrl = '';
-            });
+			cabinetsArr.$add(tempCabinet).then(function (ref) {
+				$scope.cabinetname = '';
+				$scope.selectedCat = '';
+				$scope.selectedStyle = '';
+				$scope.tempCabinetColors = '';
+				$scope.cabinetWidth.min = '';
+				$scope.cabinetWidth.max = '';
+				$scope.cabinetHeight.min = '';
+				$scope.cabinetHeight.max = '';
+				$scope.cabinetDepth.min = '';
+				$scope.cabinetDepth.max = '';
+				$scope.cabinetprice = '';
+				$scope.cabinetImageUrl = '';
+			});
 
-        }; //addCabinet
+		}; //addCabinet
 
-        $scope.addCategory = function () {
-            var imgUrl = 'http://placehold.it/190x305';
-            $scope.categoryImageUrl = imgUrl;
-            var tempCategory = {
-                name: $scope.newCategoryName,
-                imageUrl: $scope.categoryImageUrl,
-                date: Firebase.ServerValue.TIMESTAMP
-            };
-            cabinetCategoriesArr.$add(tempCategory).then(function (ref) {
-                $scope.newCategoryName = '';
-                $scope.categoryImageUrl = '';
-            });
-        }; //addCategory
+		$scope.cabinetWishList = [];
 
-        $scope.addStyle = function () {
-            var imgUrl = 'http://placehold.it/190x305';
-            $scope.styleImageUrl = imgUrl;
-            var tempStyle = {
-                category: $scope.newStyleCategory,
-                name: $scope.newStyleName,
-                imageUrl: $scope.styleImageUrl,
-                date: Firebase.ServerValue.TIMESTAMP
-            };
-            cabinetStylesArr.$add(tempStyle).then(function (ref) {
-                $scope.newStyleName = '';
-                $scope.styleImageUrl = '';
-            });
-        }; //addStyle
+		$scope.addToWishList = function (thisCabinet) {
+			$scope.cabinetWishList.push(thisCabinet);
+		}; // addToWishList
 
-        $scope.addColor = function () {
-            var imgUrl = 'http://placehold.it/190x305';
-            $scope.colorImageUrl = imgUrl;
-            var tempColor = {
-                name: $scope.newColorName,
-                imageUrl: $scope.colorImageUrl,
-                date: Firebase.ServerValue.TIMESTAMP
-            };
-            cabinetColorsArr.$add(tempColor).then(function (ref) {
-                $scope.newColorName = '';
-                $scope.colorImageUrl = '';
-            });
-        }; //addStyle
+		$scope.removeFromWishList = function (thisCabinet) {
+			$('body').removeClass('modal-open');
+			$('.modal-backdrop').remove();
+			$scope.cabinetWishList.pop(thisCabinet);
+		};
 
-        $scope.getUniqueCategories = function (field) {
-            $scope.tempCategories = [];
-            angular.forEach(cabinetCategoriesArr, function (value, key) {
-                if (($scope.tempCategories.length < 1) || ($.inArray(value, $scope.tempCategories) < 0)) {
-                    if (field === 'name' || !field) {
-                        $scope.tempCategories.push(value.name);
-                    }
-                }
-            });
-            return $scope.tempCategories;
-        }; // getUniqueCategories
+		$scope.addCategory = function () {
+			var imgUrl = 'http://placehold.it/190x305';
+			$scope.categoryImageUrl = imgUrl;
+			var tempCategory = {
+				name: $scope.newCategoryName,
+				imageUrl: $scope.categoryImageUrl,
+				date: Firebase.ServerValue.TIMESTAMP
+			};
+			cabinetCategoriesArr.$add(tempCategory).then(function (ref) {
+				$scope.newCategoryName = '';
+				$scope.categoryImageUrl = '';
+			});
+		}; //addCategory
+
+		$scope.addStyle = function () {
+			var imgUrl = 'http://placehold.it/190x305';
+			$scope.styleImageUrl = imgUrl;
+			var tempStyle = {
+				category: $scope.newStyleCategory,
+				name: $scope.newStyleName,
+				imageUrl: $scope.styleImageUrl,
+				date: Firebase.ServerValue.TIMESTAMP
+			};
+			cabinetStylesArr.$add(tempStyle).then(function (ref) {
+				$scope.newStyleName = '';
+				$scope.styleImageUrl = '';
+			});
+		}; //addStyle
+
+		$scope.addColor = function () {
+			var imgUrl = 'http://placehold.it/190x305';
+			$scope.colorImageUrl = imgUrl;
+			var tempColor = {
+				name: $scope.newColorName,
+				imageUrl: $scope.colorImageUrl,
+				date: Firebase.ServerValue.TIMESTAMP
+			};
+			cabinetColorsArr.$add(tempColor).then(function (ref) {
+				$scope.newColorName = '';
+				$scope.colorImageUrl = '';
+			});
+		}; //addStyle
+
+		$scope.getUniqueCategories = function (field) {
+			$scope.tempCategories = [];
+			angular.forEach(cabinetCategoriesArr, function (value, key) {
+				if (($scope.tempCategories.length < 1) || ($.inArray(value, $scope.tempCategories) < 0)) {
+					if (field === 'name' || !field) {
+						$scope.tempCategories.push(value.name);
+					}
+				}
+			});
+			return $scope.tempCategories;
+		}; // getUniqueCategories
 
 
-        $scope.getUniqueStyles = function (selectedCategory) {
-            $scope.tempStyles = [];
-            angular.forEach(cabinetStylesArr, function (value, key) {
-                if (($scope.tempStyles.length < 1) || ($.inArray(value, $scope.tempStyles) < 0)) {
-                    if (selectedCategory) {
-                        if (value.category === selectedCategory) {
-                            $scope.tempStyles.push(value.name);
-                        }
-                    } else {
-                        $scope.tempStyles.push(value.name);
-                    }
-                }
-            });
-            return $scope.tempStyles;
-        }; // getUniqueStyles
+		$scope.getUniqueStyles = function (selectedCategory) {
+			$scope.tempStyles = [];
+			angular.forEach(cabinetStylesArr, function (value, key) {
+				if (($scope.tempStyles.length < 1) || ($.inArray(value, $scope.tempStyles) < 0)) {
+					if (selectedCategory) {
+						if (value.category === selectedCategory) {
+							$scope.tempStyles.push(value.name);
+						}
+					} else {
+						$scope.tempStyles.push(value.name);
+					}
+				}
+			});
+			return $scope.tempStyles;
+		}; // getUniqueStyles
 
-        $scope.setCat = function (key) {
-            $scope.selectedCat = key;
-        };
+		$scope.setCat = function (key) {
+			$scope.selectedCat = key;
+		};
 
-        $scope.setStyle = function (key) {
-            $scope.selectedStyle = key;
-        };
+		$scope.setStyle = function (key) {
+			$scope.selectedStyle = key;
+		};
 
-        $scope.setColor = function (key) {
-            $scope.selectedColor = key;
-        };
+		$scope.setColor = function (key) {
+			$scope.selectedColor = key;
+		};
 
-        $scope.removeCabinet = function (key) {
-            $('body').removeClass('modal-open');
-            $('.modal-backdrop').remove();
-            cabinetsArr.$remove(key);
-        };
+		$scope.removeCabinet = function (key) {
+			$('body').removeClass('modal-open');
+			$('.modal-backdrop').remove();
+			cabinetsArr.$remove(key);
+		};
 
-        $scope.removeCategory = function (key) {
-            $('body').removeClass('modal-open');
-            $('.modal-backdrop').remove();
-            cabinetCategoriesArr.$remove(key);
-        };
+		$scope.removeCategory = function (key) {
+			$('body').removeClass('modal-open');
+			$('.modal-backdrop').remove();
+			cabinetCategoriesArr.$remove(key);
+		};
 
-        $scope.removeStyle = function (key) {
-            $('body').removeClass('modal-open');
-            $('.modal-backdrop').remove();
-            cabinetStylesArr.$remove(key);
-        };
+		$scope.removeStyle = function (key) {
+			$('body').removeClass('modal-open');
+			$('.modal-backdrop').remove();
+			cabinetStylesArr.$remove(key);
+		};
 
-        $scope.removeColor = function (key) {
-            $('body').removeClass('modal-open');
-            $('.modal-backdrop').remove();
-            cabinetColorsArr.$remove(key);
-        };
+		$scope.removeColor = function (key) {
+			$('body').removeClass('modal-open');
+			$('.modal-backdrop').remove();
+			cabinetColorsArr.$remove(key);
+		};
 
 	}]);
