0b88238be0226c370c107ca5597b7462a49b8e5f
Raz <raz.chiriac@gmail.com>
2015-12-19 18:33:11 -0500

    stripe updates

--- builds/development/views/checkout.php
+++ builds/development/views/checkout.php
@@ -1,3 +1,11 @@
+<script src="https://js.stripe.com/v1/"></script>
+
+<script>
+	Stripe.setPublishableKey('pk_test_PTQxPyzUAucunSs2MkXAuaPo'); // Test key!
+</script>
+
+<script src="buy-controller.js"></script>
+<h2>Checkout Page</h2>
 <div class="row" ng-controller="CabinetsController" ng-init="cartInit()">
 	<h4>Your Shopping Cart</h4>
 	<table class="table table-hover">
@@ -97,13 +105,67 @@
 			<tr>
 				<td colspan="12">
 					<h2 class="pull-right">Total: {{cartPrice() | currency}} </h2>
+					<input id="cartPrice" type="hidden" value="{{cartPrice()}}">
 				</td>
 			</tr>
 		</tbody>
 	</table>
-	<div class="pull-right">
-		<script src="https://checkout.stripe.com/checkout.js"></script>
-		<button id="customButton" class="btn btn-primary" ng-click="purchase()">Purchase <span class="glyphicon glyphicon-chevron-right"></span></button>
-	</div>
-
 </div>
+
+
+<h2>Payment Form</h2>
+
+<form id="buy-form" method="post" action="javascript:">
+
+	<p class="form-label">First Name:</p>
+	<input class="text" id="first-name" spellcheck="false"></input>
+
+	<p class="form-label">Last Name:</p>
+	<input class="text" id="last-name" spellcheck="false"></input>
+
+	<p class="form-label">Email Address:</p>
+	<input class="text" id="email" spellcheck="false"></input>
+
+	<p class="form-label">Credit Card Number:</p>
+	<input class="text" id="card-number" autocomplete="off"></input>
+
+	<p class="form-label">Expiration Date:</p>
+	<select id="expiration-month">
+		<option value="1">January</option>
+		<option value="2">February</option>
+		<option value="3">March</option>
+		<option value="4">April</option>
+		<option value="5">May</option>
+		<option value="6">June</option>
+		<option value="7">July</option>
+		<option value="8">August</option>
+		<option value="9">September</option>
+		<option value="10">October</option>
+		<option value="11">November</option>
+		<option value="12">December</option>
+	</select>
+
+	<select id="expiration-year">
+		<option value="2015">2015</option>
+		<option value="2016">2016</option>
+		<option value="2017">2017</option>
+		<option value="2018">2018</option>
+		<option value="2019">2019</option>
+		<option value="2020">2020</option>
+		<option value="2021">2021</option>
+		<option value="2022">2022</option>
+		<option value="2023">2023</option>
+		<option value="2024">2024</option>
+		<option value="2025">2025</option>
+		<option value="2026">2026</option>
+		<option value="2027">2027</option>
+		<option value="2028">2028</option>
+		<option value="2029">2029</option>
+		<option value="2030">2030</option>
+	</select>
+
+	<p class="form-label">CVC:</p>
+	<input class="text" id="card-security-code" autocomplete="off"></input>
+
+	<input id="buy-submit-button" type="submit" value="Place This Order »"></input>
+</form>
